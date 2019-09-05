752a87ea41e0b1d2e743e68e6c35392951904077
Sushnata1 <coolsushnata@gmail.com>
2019-09-05 22:06:37 +0530

    first commit
    Signed-off-by: Sushnata1 <coolsushnata@gmail.com>

---
+++ json.js
@@ -0,0 +1,5 @@
+function save() {
+    var a = document.getElementById("t1").value;
+    //var b = document.getElementById("f1").element[0].value;
+    document.write("hello " + a);
+}
---
+++ main.js
@@ -0,0 +1,4 @@
+function play() {
+    document.write("<br>Playing ... hello<br>");
+    document.write("<br><a href=index.html>back</a>");
+}
