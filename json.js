700430bdd5df34c8737c1ce8ef05b38979b114f3
Sushnata1 <coolsushnata@gmail.com>
2019-09-08 11:04:27 +0530

    commit after making private
    Signed-off-by: Sushnata1 <coolsushnata@gmail.com>

--- json.js
+++ json.js
@@ -2,5 +2,5 @@ function save() {
     var a = document.getElementById("t1").value;
     //var b = document.getElementById("f1").element[0].value;
     document.write("hello " + a);
-    //try
+    //try life
 }
