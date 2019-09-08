ab8706ba25df51c077074e55a216e75c12ca12e6
Sushnata1 <coolsushnata@gmail.com>
2019-09-08 22:18:46 +0530

    game 1 started
    Signed-off-by: Sushnata1 <coolsushnata@gmail.com>

--- sketch.js
+++ sketch.js
@@ -1,6 +1,18 @@
 function setup() {
-    createCanvas(800, 450);
+    w = parseFloat(document.getElementById("game").style.width) * 0.01 * windowWidth;
+    h = parseFloat(document.getElementById("game").style.height) * 0.01 * windowWidth;
+    console.log(w + "$" + h);
+    var canvas = createCanvas(w, h);
+    canvas.parent("game");
     background(0);
+    noStroke();
 }
 
-function draw() {}
+
+function draw() {
+    clear();
+    background(0);
+    rectMode(CENTER);
+    fill(random(20, 255), random(20, 255), random(20, 255));
+    rect(mouseX, mouseY, 40, 40);
+}
